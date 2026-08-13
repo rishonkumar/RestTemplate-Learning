0accb97f0ea8768f069c06683dee1d51cfe96106
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-08-13 12:28:20 +0530

    Enhance RestTemplate configuration with Apache HttpClient for improved HTTP request handling
    - Added Apache HttpClient dependency to `pom.xml`.
    - Updated `RestTemplateConfig` to utilize `CloseableHttpClient` with custom request configurations, including connection and response timeouts.
    - Integrated `HttpComponentsClientHttpRequestFactory` for better HTTP client management in `RestTemplate`.

--- order-integration-service/src/main/java/com/rishon/orderintegration/config/RestTemplateConfig.java
+++ order-integration-service/src/main/java/com/rishon/orderintegration/config/RestTemplateConfig.java
@@ -1,7 +1,14 @@
 package com.rishon.orderintegration.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.rishon.orderintegration.interceptor.LoggingInterceptor;
@@ -15,7 +22,16 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
 
-        RestTemplate restTemplate = new RestTemplate();
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(2, null).setResponseTimeout(Timeout.ofSeconds(5)).build();
+
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().build();
+
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).setConnectionManager(connectionManager).build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+
+        RestTemplate restTemplate = new RestTemplate(factory);
         restTemplate.getInterceptors().add(new LoggingInterceptor());
         return restTemplate;
     }
