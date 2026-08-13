19dd73c139bcd6fd93dd56a6764db8a51ed5fc23
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-08-13 10:31:18 +0530

    Add LoggingInterceptor for request/response logging in RestTemplate
    - Implemented LoggingInterceptor to log outgoing requests and downstream responses.
    - Updated RestTemplate configuration to include the LoggingInterceptor for enhanced monitoring of HTTP interactions.

--- order-integration-service/src/main/java/com/rishon/orderintegration/config/RestTemplateConfig.java
+++ order-integration-service/src/main/java/com/rishon/orderintegration/config/RestTemplateConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.rishon.orderintegration.interceptor.LoggingInterceptor;
+
 /**
  * Spring configuration for RestTemplate, HTTP request factory, and timeouts.
  */
@@ -12,6 +14,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new LoggingInterceptor());
+        return restTemplate;
     }
 }
--- order-integration-service/src/main/java/com/rishon/orderintegration/interceptor/LoggingInterceptor.java
+++ order-integration-service/src/main/java/com/rishon/orderintegration/interceptor/LoggingInterceptor.java
@@ -1,7 +1,35 @@
 package com.rishon.orderintegration.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
 /**
  * RestTemplate ClientHttpRequestInterceptor for request/response logging (to be implemented later).
  */
-public class LoggingInterceptor {
+@Slf4j
+public class LoggingInterceptor implements ClientHttpRequestInterceptor{
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+
+                // Before the request goes to the downstream API
+                log.info("Outgoing Request: {} {}", request.getMethod(), request.getURI());
+
+                // Actually execute the HTTP request
+                ClientHttpResponse response = execution.execute(request, body);
+
+                // After the downstream API responds
+
+                log.info("Downstream response status: {}",
+
+                response.getStatusCode());
+                return response;
+
+    }
 }
